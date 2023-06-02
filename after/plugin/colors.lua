c49b4462fe2270ab8926751221a6d79838ad8686
AidenGariepy89 <79092752+AidenGariepy89@users.noreply.github.com>
2023-06-02 14:47:58 -0400

    Feat: catppuccin

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,10 @@
 function Color(style)
-    style = style or 'rose-pine'
+    style = style or 'catppuccin-mocha'
     vim.cmd.colorscheme(style)
 
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
--- Color()
-Color('catppuccin-mocha')
+Color()
+-- Color('rose-pine')
