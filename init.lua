d24c881caa58e2599b1716aa478541598fff6369
AidenGariepy89 <79092752+AidenGariepy89@users.noreply.github.com>
2023-12-15 13:15:59 -0500

    fix: Catppuccin configuration

--- init.lua
+++ init.lua
@@ -51,9 +51,17 @@ require("lazy").setup({
 
     {
         "catppuccin/nvim",
+        name = "catppuccin",
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme("catppuccin-mocha")
+            require("catppuccin").setup({
+                flavour = "mocha",
+                no_italic = true,
+                dim_inactive = {
+                    enabled = true,
+                },
+            })
+            vim.cmd.colorscheme("catppuccin")
         end,
     },
 
